0ab985d0b91c5c1c44b93f9b60b6c647267831f0
lanou3g <1101933654@qq.com>
2016-03-25 11:57:37 +0800

    dsfgg

--- Test2_1250/Test2_1250/ViewController.m
+++ Test2_1250/Test2_1250/ViewController.m
@@ -18,6 +18,9 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"sdf");
+    
+    
+    NSLog(@"test1");
 }
 
 - (void)didReceiveMemoryWarning {
